3254fe0dfce0527e47bf745752afdd1d737e5afc
manhanh3011 <tranmanhanh3011@gmail.com>
2025-12-03 16:45:13 +0700

    thay doi bang

--- index.php
+++ index.php
@@ -54,8 +54,8 @@ match ($act) {
     // Xử lý route có tham số
     default => match($route) {
         'tours' => match($param) {
-            'edit' => $tourController->edit($actParts[2] ?? null),
-            default => $homeController->notFound()
+        'edit' => $tourController->edit($actParts[2] ?? null),
+        default => $homeController->notFound()
         },
         'tours/update' => $tourController->update(),
         default => $homeController->notFound()
